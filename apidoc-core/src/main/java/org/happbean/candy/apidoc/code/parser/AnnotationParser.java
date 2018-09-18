6e8a6ef1cd613eac1218fe574392920a4d6acdf3
bean <18222997200@163.com>
2018-09-18 20:40:36 +0800

    增加注解解析器AnnotationParser

--- apidoc-core/src/main/java/org/happbean/candy/apidoc/code/parser/AnnotationParser.java
+++ apidoc-core/src/main/java/org/happbean/candy/apidoc/code/parser/AnnotationParser.java
@@ -1,8 +1,12 @@
 package org.happbean.candy.apidoc.code.parser;
 
+import org.happbean.candy.apidoc.factory.annotation.AnnotationFactory;
 import org.happbean.candy.apidoc.factory.annotation.Api;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.*;
 
 /**
  * @author wgt
@@ -11,10 +15,81 @@ import java.lang.annotation.Annotation;
  **/
 public class AnnotationParser {
 
-    public static boolean checkApiAnnotation(Class clazz) {
+    public static boolean isApiClass(Class clazz) {
 
         Annotation annotation = clazz.getAnnotation(Api.class);
 
         return annotation == null ? false : true;
     }
+
+    public static boolean checkAnnotation(Annotation annotation) {
+
+        List<Class> annotations = AnnotationFactory.getAnnotations();
+
+        return (annotation == null || !annotations.contains(annotation.annotationType()));
+    }
+
+    public static final Annotation[] getClassAnnotations(Class clazz) {
+
+        Annotation[] annotations = clazz.getDeclaredAnnotations();
+
+        if (annotations == null) {
+            return null;
+        }
+        annotations = Arrays.stream(annotations)
+                .filter(annotation -> checkAnnotation(annotation)).toArray(Annotation[]::new);
+
+        return annotations;
+    }
+
+    public static final Map<String, Annotation[]> getFieldAnnotations(Class clazz) {
+
+        Field[] fields = clazz.getDeclaredFields();
+
+        if (fields == null) {
+            return null;
+        }
+
+        Map<String, Annotation[]> annotationMap = new HashMap<>();
+
+        Arrays.stream(fields).forEach(field -> {
+
+            if (!field.isAccessible()) {
+                field.setAccessible(true);
+            }
+
+            Annotation[] annotations = field.getAnnotations();
+
+            annotations = Arrays.stream(annotations)
+                    .filter(annotation -> checkAnnotation(annotation)).toArray(Annotation[]::new);
+
+            annotationMap.put(field.getName(), annotations);
+        });
+
+        return annotationMap;
+    }
+
+    public static final Map<String, Annotation[]> getMethodAnnotations(Class clazz) {
+
+        Method[] methods = clazz.getMethods();
+
+        if (methods == null) {
+            return null;
+        }
+
+        Map<String, Annotation[]> annotationMap = new HashMap<>();
+
+        Arrays.stream(methods).forEach(method -> {
+
+            Annotation[] annotations = method.getAnnotations();
+
+            annotations = Arrays.stream(annotations)
+                    .filter(annotation -> checkAnnotation(annotation)).toArray(Annotation[]::new);
+
+            annotationMap.put(method.toGenericString(), annotations);
+        });
+
+        return annotationMap;
+    }
+
 }
