a9468b77365b1e5a363c825416bf4c9e21f41fbd
bean <18222997200@163.com>
2018-09-25 16:19:12 +0800

    增加xml配置文件解析器

---
+++ apidoc-core/src/main/java/org/happbean/candy/apidoc/config/xml/XmlConfPaser.java
@@ -0,0 +1,37 @@
+package org.happbean.candy.apidoc.config.xml;
+
+import org.happbean.candy.apidoc.config.xml.xmlpo.*;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author wgt
+ * @date 2018-09-25
+ * @description
+ **/
+public class XmlConfPaser {
+
+    public static Object xmlToBean(String xmlPath, Class<?> load) {
+        Object object = null;
+        try {
+            JAXBContext context = JAXBContext.newInstance(load);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            object = unmarshaller.unmarshal(new File(xmlPath));
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+    public static CandyConfiguration pase() {
+        String xmlPath = "/Users/wgt/IdeaProjects/java/apidocCandy/apidoc-core/src/test/java/conf/candyconf.xml";
+        Object object = xmlToBean(xmlPath, CandyConfiguration.class);
+        CandyConfiguration configuration = (CandyConfiguration) object;
+        return configuration;
+    }
+}
