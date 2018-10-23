3173597a46fdc96b2570e762efd0dd2b2d010c9b
bean <18222997200@163.com>
2018-10-23 15:28:56 +0800

    增加string工具类

---
+++ apidoc-core/src/main/java/org/happbean/candy/apidoc/util/StringUtil.java
@@ -0,0 +1,204 @@
+package org.happbean.candy.apidoc.util;
+
+/**
+ * @author wgt
+ * @date 2018-10-23
+ * @description
+ **/
+public class StringUtil {
+
+    /**
+     * 字符串是否为空白 空白的定义如下： <br>
+     * 1、为null <br>
+     * 2、为不可见字符（如空格）<br>
+     * 3、""<br>
+     *
+     * @param str 被检测的字符串
+     * @return 是否为空
+     */
+    public static boolean isBlank(CharSequence str) {
+        int length;
+
+        if ((str == null) || ((length = str.length()) == 0)) {
+            return true;
+        }
+
+        for (int i = 0; i < length; i++) {
+            // 只要有一个非空字符即为非空字符串
+            if (false == Character.isWhitespace(str.charAt(i)) || Character.isSpaceChar(str.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 字符串是否为非空白 空白的定义如下： <br>
+     * 1、不为null <br>
+     * 2、不为不可见字符（如空格）<br>
+     * 3、不为""<br>
+     *
+     * @param str 被检测的字符串
+     * @return 是否为非空
+     */
+    public static boolean isNotBlank(CharSequence str) {
+        return false == isBlank(str);
+    }
+
+    /**
+     * 字符串是否为空，空的定义如下:<br>
+     * 1、为null <br>
+     * 2、为""<br>
+     *
+     * @param str 被检测的字符串
+     * @return 是否为空
+     */
+    public static boolean isEmpty(CharSequence str) {
+        return str == null || str.length() == 0;
+    }
+
+    /**
+     * 字符串是否为非空白 空白的定义如下： <br>
+     * 1、不为null <br>
+     * 2、不为""<br>
+     *
+     * @param str 被检测的字符串
+     * @return 是否为非空
+     */
+    public static boolean isNotEmpty(CharSequence str) {
+        return false == isEmpty(str);
+    }
+
+
+    /**
+     * 是否以指定字符串开头<br>
+     * 如果给定的字符串和开头字符串都为null则返回true，否则任意一个值为null返回false
+     *
+     * @param str          被监测字符串
+     * @param prefix       开头字符串
+     * @param isIgnoreCase 是否忽略大小写
+     * @return 是否以指定字符串开头
+     */
+    public static boolean startWith(CharSequence str, CharSequence prefix, boolean isIgnoreCase) {
+        if (null == str || null == prefix) {
+            if (null == str && null == prefix) {
+                return true;
+            }
+            return false;
+        }
+
+        if (isIgnoreCase) {
+            return str.toString().toLowerCase().startsWith(prefix.toString().toLowerCase());
+        } else {
+            return str.toString().startsWith(prefix.toString());
+        }
+    }
+
+    /**
+     * 去掉字符包装，如果未被包装则返回原字符串
+     *
+     * @param str    字符串
+     * @param prefix 前置字符
+     * @param suffix 后置字符
+     * @return 去掉包装字符的字符串
+     * @since 4.0.1
+     */
+    public static String unWrap(CharSequence str, char prefix, char suffix) {
+        if (str.charAt(0) == prefix && str.charAt(str.length() - 1) == suffix) {
+            return sub(str, 1, str.length() - 1);
+        }
+        return str.toString();
+    }
+
+    /**
+     * 指定字符串是否被包装
+     *
+     * @param str        字符串
+     * @param prefixChar 前缀
+     * @param suffixChar 后缀
+     * @return 是否被包装
+     */
+    public static boolean isWrap(CharSequence str, char prefixChar, char suffixChar) {
+        if (null == str) {
+            return false;
+        }
+
+        return str.charAt(0) == prefixChar && str.charAt(str.length() - 1) == suffixChar;
+    }
+
+    /**
+     * 替换指定字符串的指定区间内字符为固定字符
+     *
+     * @param str          字符串
+     * @param startInclude 开始位置（包含）
+     * @param endExclude   结束位置（不包含）
+     * @param replacedChar 被替换的字符
+     * @return 替换后的字符串
+     * @since 3.2.1
+     */
+    public static String replace(CharSequence str, int startInclude, int endExclude, char replacedChar) {
+        if (isEmpty(str)) {
+            return str(str);
+        }
+        final int strLength = str.length();
+        if (startInclude > strLength) {
+            return str(str);
+        }
+        if (endExclude > strLength) {
+            endExclude = strLength;
+        }
+        if (startInclude > endExclude) {
+            // 如果起始位置大于结束位置，不替换
+            return str(str);
+        }
+
+        final char[] chars = new char[strLength];
+        for (int i = 0; i < strLength; i++) {
+            if (i >= startInclude && i < endExclude) {
+                chars[i] = replacedChar;
+            } else {
+                chars[i] = str.charAt(i);
+            }
+        }
+        return new String(chars);
+    }
+
+    public static String str(CharSequence cs) {
+        return null == cs ? null : cs.toString();
+    }
+
+    public static String sub(CharSequence string, int fromIndex, int toIndex) {
+        int len = string.length();
+
+        if (fromIndex < 0) {
+            fromIndex = len + fromIndex;
+            if (fromIndex < 0) {
+                fromIndex = 0;
+            }
+        } else if (fromIndex > len) {
+            fromIndex = len;
+        }
+
+        if (toIndex < 0) {
+            toIndex = len + toIndex;
+            if (toIndex < 0) {
+                toIndex = len;
+            }
+        } else if (toIndex > len) {
+            toIndex = len;
+        }
+
+        if (toIndex < fromIndex) {
+            int tmp = fromIndex;
+            fromIndex = toIndex;
+            toIndex = tmp;
+        }
+
+        if (fromIndex == toIndex) {
+            return "";
+        }
+
+        return string.toString().substring(fromIndex, toIndex);
+    }
+}
