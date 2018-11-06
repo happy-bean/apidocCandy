5571fd6bb0fbb5b7acf446987a753d64636b3c18
bean <18222997200@163.com>
2018-11-06 11:32:25 +0800

    demo修改

--- apidoc-core/src/main/java/org/happbean/candy/apidoc/demo/User.java
+++ apidoc-core/src/main/java/org/happbean/candy/apidoc/demo/User.java
@@ -10,8 +10,8 @@ import org.happbean.candy.apidoc.factory.annotation.response.Result;
  * @description
  **/
 @Data
-@Result
-public class User {
+@Result(name = "user",value = "user",desc = "user info")
+public class User<T> {
 
     @Param(name = "num", required = true, desc = "account number")
     @Result(name = "num", value = "apidoccandy.@email.com", desc = "account number")
@@ -21,7 +21,7 @@ public class User {
     @Result(name = "pass", value = "apidoccandy", desc = "password")
     private String pass;
 
-    @Param(name = "name", required = true, desc = "ser name")
+    @Param(name = "name", required = true, desc = "user name")
     @Result(name = "name", value = "apidoccandy", desc = "user name")
     private String name;
 
