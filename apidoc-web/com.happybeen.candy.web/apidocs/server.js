224bf99cde2d0266c5ecdf40e35251bed70783a3
bean <18222997200@163.com>
2019-01-25 12:00:08 +0800

    mysql 模块引入

--- apidoc-web/com.happybeen.candy.web/apidocs/server.js
+++ apidoc-web/com.happybeen.candy.web/apidocs/server.js
@@ -2,16 +2,23 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var apicandy = "index.html"
+// 服务信息
+var server_port = '8090';
 
-//服务信息
-var port = 8090
+var mysql_config = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '123456',
+    database: 'test'
+}
+
 
 http.createServer(function (request, response) {
     var request_path = url.parse(request.url).pathname;
     var request_method = request.method
 
-    console.log("Request for " + request_path + " received,,method:" + request_method);
+    console.log("Request for " + request_path + " received,method:" + request_method);
     if ("GET" == request_method) {
         doGet(request, response);
     } else if ("POST" == request_method) {
@@ -20,12 +27,23 @@ http.createServer(function (request, response) {
         response.writeHead(404, {'Content-Type': 'text/html'});
         response.end();
     }
-}).listen(port);
+}).listen(server_port);
 
 printBanner();
 
+function postRouteAndResult(path_name) {
+    var data = "";
+    if (path_name == "/action") {
+        data = action();
+    } else {
+        data = action();
+    }
+    return data;
+}
+
 function doGet(request, response) {
-    var path_name = url.parse(request.url).pathname;
+    var path_name = "/index.html"
+
     fs.readFile(path_name.substr(1), function (err, data) {
         if (err) {
             console.log(err);
@@ -35,10 +53,9 @@ function doGet(request, response) {
         } else {
             // HTTP 状态码: 200 : OK
             // Content Type: text/plain
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(data.toString());
+            response.write(data);
         }
-        //  发送响应数据
+        // 发送响应数据
         response.end();
     });
 }
@@ -46,6 +63,7 @@ function doGet(request, response) {
 function doPost(request, response) {
     // 定义了一个post变量，用于暂存请求体的信息
     var post = '';
+    var path_name = url.parse(request.url).pathname;
 
     // 通过req的data事件监听函数，每当接受到请求体的数据，就累加到post变量中
     request.on('data', function (chunk) {
@@ -54,28 +72,22 @@ function doPost(request, response) {
 
     // 在end事件触发后，通过querystring.parse将post解析为真正的POST请求格式，然后向客户端返回。
     request.on('end', function () {
-        post = querystring.parse(post);
-        response.end(util.inspect(post));
+        var data = postRouteAndResult(path_name);
+        response.write(data.toString())
+        response.end();
     });
 }
 
-function printBanner(){
-    console.log("    _     ___   ___    ___     _     _  _   ___   __   __")
-    console.log("   /_\\   | _ \\ |_ _|  / __|   /_\\   | \\| | |   \\  \\ \\ / /");
-    console.log("  / _ \\  |  _/  | |  | (__   / _ \\  | .` | | |) |  \\ V /");
-    console.log(" /_/ \\_\\ |_|   |___|  \\___| /_/ \\_\\ |_|\\_| |___/    |_|  ");
-    console.log("                                                  v:1.0.0")
-    console.log('Api Candy Web server running at http://127.0.0.1:' + port + '/');
+function printBanner() {
+    fs.readFile('./banner/banner.txt', 'utf8', function (err, data) {
+        console.log(data);
+    });
+    console.log('Api Candy Web server running at http://127.0.0.1:' + server_port + '/');
 }
 
 //mysql
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'test'
-});
+var connection = mysql.createConnection(mysql_config);
 
 //sql
 var select_all_api = "SELECT * FROM apicandy_action";
@@ -85,19 +97,26 @@ var select_result_by_action_id = "SELECT * FROM apicandy_result WHERE action_id 
 
 function excuteSelectSql(sql, sql_params) {
     connection.connect();
-    var sql_result;
+    var sql_result = null;
+    console.log('--------------------------SELECT----------------------------');
     connection.query(sql, sql_params, function (err, result) {
         if (err) {
             console.log('[SELECT ERROR] - ', err.message);
             return;
         }
         sql_result = result;
-        console.log('--------------------------SELECT----------------------------');
         console.log('RESULT:', result);
-        console.log('-----------------------------------------------------------------\n\n');
     });
     connection.end();
+    console.log('-----------------------------------------------------------------\n\n');
     return sql_result
 }
 
+function action() {
+    var sql_params = [];
+    var result = excuteSelectSql(select_all_api, sql_params);
+    if (result == null) {
+        result = "";
+    }
+}
 
